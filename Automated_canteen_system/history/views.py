4232e3d0e420813ce88297b3adb76af375ff3fc9
Vishwanth14 <60568015+Vishwanth14@users.noreply.github.com>
2021-04-12 18:19:16 +0530

    Update views.py

--- Automated_canteen_system/history/views.py
+++ Automated_canteen_system/history/views.py
@@ -3,6 +3,7 @@ from cart.models import Cart
 from .models import History
 from django.contrib.auth.models import User
 from django.contrib import auth
+#Shows details about statistics of orders made by user in the canteen so far.
 def hist(request):
     his = History.objects.filter(customer=request.user).reverse()
     return render(request, 'history/history.html', {'his':his})
