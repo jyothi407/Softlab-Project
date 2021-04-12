f1e2ec451baa6b5c77234b653b232b360347afdb
vamcln <vamsi171717@gmail.com>
2021-04-12 17:04:00 +0530

    updated tree structure of project

--- history/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from cart.models import Cart
-from .models import History
-from django.contrib.auth.models import User
-from django.contrib import auth
-def hist(request):
-    his = History.objects.filter(customer=request.user).reverse()
-    return render(request, 'history/history.html', {'his':his})
--- transaction/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = [
-    path('cart', views.money, name='payment'),
-    path('home', views.res, name='pay2'),
-]
