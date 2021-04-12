fbf344feed1f367abcc406e6ce5b6d5464dd3991
Navyakumar98 <64352389+Navyakumar98@users.noreply.github.com>
2021-04-12 23:53:57 +0530

    Update views.py

--- Automated_canteen_system/history/views.py
+++ Automated_canteen_system/history/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from cart.models import Cart
 from .models import History
 from django.contrib.auth.models import User
-from django.contrib import auth
-def hist(request):
-    his = History.objects.filter(customer=request.user).reverse()
-    return render(request, 'history/history.html', {'his':his})
+from django.contrib import auth#necessary librarys imported
+def hist(request):#hist function
+    his = History.objects.filter(customer=request.user).reverse()#data retrival from postgresql
+    return render(request, 'history/history.html', {'his':his})# returns render
